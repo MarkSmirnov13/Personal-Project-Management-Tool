cc529916f29a169171ecfe02e8960bd705ec2f76
MarkSmirnov13 <marksmirnov13@gmail.com>
2019-03-11 15:50:00 +0100

    Add project id exception and handler

--- PPMToolFullStack/src/main/java/com/markandmax/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/markandmax/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.markandmax.ppmtool.services;
 
 import com.markandmax.ppmtool.domain.Project;
+import com.markandmax.ppmtool.exceptions.ProjectIdException;
 import com.markandmax.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 }
