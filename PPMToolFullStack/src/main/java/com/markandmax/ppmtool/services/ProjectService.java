642c36e10d57806aeaf42de039b091e36b74df6d
MarkSmirnov13 <marksmirnov13@gmail.com>
2019-03-12 00:23:11 +0100

    Added findProjectById function

--- PPMToolFullStack/src/main/java/com/markandmax/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/markandmax/ppmtool/services/ProjectService.java
@@ -19,4 +19,14 @@ public class ProjectService {
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
         }
     }
+
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
+        }
+
+        return project;
+    }
 }
