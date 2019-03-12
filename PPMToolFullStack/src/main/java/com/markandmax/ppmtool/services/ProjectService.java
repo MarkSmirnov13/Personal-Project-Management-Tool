9b2c7cf88f3ce67ef11a63e41cae871d145cba21
MarkSmirnov13 <marksmirnov13@gmail.com>
2019-03-12 12:26:27 +0100

    Added getAllProjects function and deleteProject function

--- PPMToolFullStack/src/main/java/com/markandmax/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/markandmax/ppmtool/services/ProjectService.java
@@ -29,4 +29,18 @@ public class ProjectService {
 
         return project;
     }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Cannot delete Project with ID '" + projectId + "'. This Project does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
 }
--- PPMToolFullStack/src/main/java/com/markandmax/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/markandmax/ppmtool/web/ProjectController.java
@@ -40,4 +40,16 @@ public class ProjectController {
 
         return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects() {
+        return projectService.findAllProjects();
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project with Id '" + projectId + "' was deleted", HttpStatus.OK);
+    }
 }
